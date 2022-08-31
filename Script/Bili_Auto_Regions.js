a804e2e3bb3bd36f568ece3a3a8f5f145ab96294
铃木一彻 <46434099+jimmymitlavigne@users.noreply.github.com>
2022-08-31 16:16:04 +0800

    Update Bili_Auto_Regions.js

--- Script/Bili_Auto_Regions.js
+++ Script/Bili_Auto_Regions.js
@@ -6,9 +6,9 @@ let run = EnvInfo();
 async function SwitchRegion(play) {
 	const Group = $.read('BiliArea_Policy') || 'Bilibili'; //Your blibli policy group name.
 	const CN = $.read('BiliArea_CN') || 'DIRECT'; //Your China sub-policy name.
-	const TW = $.read('BiliArea_TW') || 'BTW'; //Your Taiwan sub-policy name.
-	const HK = $.read('BiliArea_HK') || 'BHK'; //Your HongKong sub-policy name.
-	const DF = $.read('BiliArea_DF') || 'BHK'; //Sub-policy name used after region is blocked(e.g. url 404)
+	const TW = $.read('BiliArea_TW') || '结城明日奈'; //Your Taiwan sub-policy name.
+	const HK = $.read('BiliArea_HK') || '亚丝娜'; //Your HongKong sub-policy name.
+	const DF = $.read('BiliArea_DF') || '亚丝娜'; //Sub-policy name used after region is blocked(e.g. url 404)
 	const off = $.read('BiliArea_disabled') || ''; //WiFi blacklist(disable region change), separated by commas.
 	const current = await $.getPolicy(Group);
 	const area = (() => {
